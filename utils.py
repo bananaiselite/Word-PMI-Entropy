f67de5cb207b6f0fd5ec6c925b9fbaa3d15d4e7f
bananaiselite <dashcamp057@gmail.com>
2023-07-04 15:07:14 +0800

    stopwords新增，並且在計算把字串中的英文去除

---
+++ utils.py
@@ -0,0 +1,21 @@
+import re
+
+def fullwidth_to_halfwidth(text):
+    halfwidth_text = ""
+    for char in text:
+        code = ord(char)
+        # 全形字符範圍：65281~65374
+        if 65281 <= code <= 65374:
+            # 將全形字符轉換為對應的半形字符
+            halfwidth_char = chr(code - 65248)
+            halfwidth_text += halfwidth_char
+        else:
+            halfwidth_text += char
+            
+    return halfwidth_text
+
+def remove_english(text):
+    
+    pattern = r'[a-zA-Z]'
+    removed_text = re.sub(pattern, '', text)
+    return removed_text
