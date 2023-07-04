678302d8c0ad1b016cc01f9d260b3ea0aae3ad22
bananaiselite <dashcamp057@gmail.com>
2023-07-04 16:57:41 +0800

    更新setup檔

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setuptools.setup(
     author_email='dashcamp057@gmail.com',
     description='利用自由度和凝合度在文本中尋找可能為單詞的字串',
     packages=setuptools.find_packages(),
-    py_modules=['volcab_finding.py']
+    py_modules=['volcab_finding']
 )
