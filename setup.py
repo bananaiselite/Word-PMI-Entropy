37dd3c2d553c9da356ddaa89766c9cce845562ef
bananaiselite <dashcamp057@gmail.com>
2023-07-04 17:09:00 +0800

    更新setup檔

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name='VolcabFinding',
+    name='volcab_finging',
     version='1.0',
+    description='利用自由度和凝合度在文本中尋找可能為單詞的字串',
     author='bananaiselite',
     author_email='dashcamp057@gmail.com',
-    description='利用自由度和凝合度在文本中尋找可能為單詞的字串',
+    
     packages=setuptools.find_packages(),
-    py_modules=['volcab_finding']
+
 )
