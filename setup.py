e8c796c994ee9b406e4dc23dd362f33b5371d55d
bananaiselite <dashcamp057@gmail.com>
2023-07-04 16:10:57 +0800

    更新說明檔

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ import setuptools
 
 setuptools.setup(
     name='volcab_finding',
-    version='1.0.0',
+    version='1.0',
     author='bananaiselite',
     author_email='dashcamp057@gmail.com',
     description='利用自由度和凝合度在文本中尋找可能為單詞的字串',
     packages=setuptools.find_packages(),
+    packages=['volcab_finding'],
+    entry_points={
+        'console_scripts': [
+            'volcab_finding = volcab_finding.main:main'
+        ]}
 )
