cab6637348f91b795a72045ef5509b81502f78b9
bananaiselite <dashcamp057@gmail.com>
2023-07-04 16:38:27 +0800

    更新說明檔

--- setup.py
+++ setup.py
@@ -7,9 +7,4 @@ setuptools.setup(
     author_email='dashcamp057@gmail.com',
     description='利用自由度和凝合度在文本中尋找可能為單詞的字串',
     packages=setuptools.find_packages(),
-    packages=['volcab_finding'],
-    entry_points={
-        'console_scripts': [
-            'volcab_finding = volcab_finding.main:main'
-        ]}
 )
