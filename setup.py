0500bc850b408f68f26107acddeba02e3c5f0435
bananaiselite <dashcamp057@gmail.com>
2023-07-05 15:06:55 +0800

    setup檔案更新

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name='volcab_finging',
     version='1.0',
     install_requires=['pandas',
-                      'numpy']
+                      'numpy'],
     description='利用自由度和凝合度在文本中尋找可能為單詞的字串',
     author='bananaiselite',
     author_email='dashcamp057@gmail.com',
